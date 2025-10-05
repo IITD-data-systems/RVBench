91bfa55e4dc20ac226ed3b946dd5b661cb624048
Ayush29051 <122905924+Ayush29051@users.noreply.github.com>
2025-10-05 22:01:43 +0530

    Update database_and_query_generator.sh
    some location related changes

--- database-generation/database_and_query_generator.sh
+++ database-generation/database_and_query_generator.sh
@@ -7,30 +7,34 @@ python3 database_generator.py microsoft/MiniLM-L12-H384-uncased
 g++ index_files/ivfflat_index_generator.cpp -o index_files/ivfflat_index_generator -O3 -std=c++17 -I/path/to/faiss /path/to/faiss/build/faiss/libfaiss.a -lopenblas -lm -fopenmp
 g++ index_files/hnsw_index_generator.cpp -o index_files/hnsw_index_generator -O3 -std=c++17 -lopenblas -lm -fopenmp
 g++ index_files/hash_table_based_index_generator.cpp -o index_files/hash_table_based_index_generator -O3 -std=c++17
-g++ offsets_files/offset_calculation.cpp -o offsets_files/offset_calculation -O3 -std=c++17
+g++ offsets_files/offset_calculation.cpp -o offsets_files/offsets_calculation -O3 -std=c++17
 g++ binary_embeddings_creator.cpp -o binary_embeddings_creator -O3 -std=c++17
 
+cd index_files
 # Step 3: Create indexes for IVF and HNSW for page and text
 for t in page text; do
   for m in l2 cos; do
-    ./index_files/ivfflat_index_generator $t $m
-    ./index_files/hnsw_index_generator $t $m
+    ./ivfflat_index_generator $t $m
+    ./hnsw_index_generator $t $m
   done
 done
 
+
 # Step 4: Create hash-based indexes
-./index_files/hash_table_based_index_generator ../data_csv_files/text_csv_files/text.csv old_id_index.bin 0
-./index_files/hash_table_based_index_generator ../data_csv_files/page_csv_files/page.csv page_id_index.bin 0
-./index_files/hash_table_based_index_generator ../data_csv_files/revision_csv_files/revision_clean.csv rev_id_index.bin 0
-./index_files/hash_table_based_index_generator ../data_csv_files/revision_csv_files/revision_clean.csv rev_page_index.bin 1
+./hash_table_based_index_generator ../data_csv_files/text_csv_files/text.csv old_id_index.bin 0
+./hash_table_based_index_generator ../data_csv_files/page_csv_files/page.csv page_id_index.bin 0
+./hash_table_based_index_generator ../data_csv_files/revision_csv_files/revision_clean.csv rev_id_index.bin 0
+./hash_table_based_index_generator ../data_csv_files/revision_csv_files/revision_clean.csv rev_page_index.bin 1
 
+cd ../offsets_files
 # Step 5: Calculate offsets
-./offsets_files/offset_calculation ../data_csv_files/page_csv_files/page_extra.csv page_extra_offsets.bin
-./offsets_files/offset_calculation ../data_csv_files/page_csv_files/page.csv page_offsets.bin
-./offsets_files/offset_calculation ../data_csv_files/revision_csv_files/revision_clean.csv revision_offsets.bin
-./offsets_files/offset_calculation ../data_csv_files/text_csv_files/text.csv text_offsets.bin
-./offsets_files/offset_calculation ../data_csv_files/text_csv_files/embedding.csv text_embedding_offsets.bin
+./offsets_calculation ../data_csv_files/page_csv_files/page_extra.csv page_extra_offsets.bin
+./offsets_calculation ../data_csv_files/page_csv_files/page.csv page_offsets.bin
+./offsets_calculation ../data_csv_files/revision_csv_files/revision_clean.csv revision_offsets.bin
+./offsets_calculation ../data_csv_files/text_csv_files/text.csv text_offsets.bin
+./offsets_calculation ../data_csv_files/text_csv_files/embedding.csv text_embedding_offsets.bin
 
+cd ..
 # Step 6: Create binary embeddings
 ./binary_embeddings_creator page
 ./binary_embeddings_creator text
